acfbfa6fb220cac70992dc13c4242f5e9238b4ed
kurt <65443440+TICKurt@users.noreply.github.com>
2022-03-13 23:33:11 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 Vue.config.productionTip = false;
 Vue.use(ViewUI);               //注册iview
-import BaiduMap from 'vue-baidu-map'
+import BaiduMap from 'vue-baidu-map' // 导入百度地图包
 Vue.use(BaiduMap, {
   // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
   ak: '1pmqCda27U9Qvzsz5wDfwU0v4LVNqUNo'
