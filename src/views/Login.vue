55b438bd6469dd17244521964b9a4e95d0fe3430
Aric-Sun <zsunqian@foxmail.com>
2022-03-14 14:29:46 +0800

    add Login Logic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -50,7 +50,129 @@
   </div>
 </template>
 <script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import { SET_IDENTITY } from 'store/mutations_type';
+import {getPropertyLoginformationName,getManagerLoginFormation,getPropertyLoginformation,getManagerLoginFormationName}from'network/property';
+export default {
+  name: "Login",
+  data() {
+    return {
+      selectList: [
+        {
+          value: 'id',
+          label: '编号'
+        },
+        {
+          value: 'username',
+          label: '用户名'
+        },
+      ],
+      selectModel: 'id',
+      userRadio:'物业经理',
+      formInline: {
+        user: "",
+        password: "",
+      },
+      ruleInline: {
+        user: [
+          {
+            required: true,
+            message: "请输入账号",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur",
+          },
+          {
+            type: "string",
+            min: 0,
+            message: "最少位数",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    submit() {
+      // this.$router.push("/flowdetection");
+      // this.$emit("gotoHome");
+      // console.log(this.userRadio)
+      if(this.formInline.user == '' || this.formInline.password == ''){
+        this.$Message.error('用户名/密码不为空');
+      }else{
+        // console.log(this.formInline.user);
+        if(this.userRadio == '物业经理'){
+          if(this.selectModel == 'id'){
+            const userid = this.formInline.user;
+            const pwd = this.formInline.password;
+            getManagerLoginFormation(userid,pwd).then(res =>{
+              if(res.data.password == this.formInline.password){
+                // console.log("经理登录成功 id");
+                this.$router.push("/flowdetection");
+                this.$store.commit(SET_IDENTITY, this.userRadio);
+                this.$emit("gotoHome");
+              }
+            }).catch(err => {
+              console.log(err)
+              this.$Message.error('请检查用户编号/密码');
+            })
+          }else{
+            const username = this.formInline.user;
+            const pwd = this.formInline.password;
+            getManagerLoginFormationName(username,pwd).then(res =>{
+              if(res.data.password == this.formInline.password){
+                // console.log("经理登录成功 name");
+                this.$router.push("/flowdetection");
+                this.$store.commit(SET_IDENTITY, this.userRadio);
+                this.$emit("gotoHome");
+              }
+            }).catch(err => {
+              // console.log(err)
+              this.$Message.error('请检查用户名/密码');
+            })
+          }
+        }else if(this.userRadio == '员工'){
+          if(this.selectModel == 'id'){
+            const userid = this.formInline.user;
+            const pwd = this.formInline.password;
+            getPropertyLoginformation(userid,pwd).then(res =>{
+              if(res.data.password == this.formInline.password){
+                // console.log("员工登录成功");
+                this.$router.push("/flowdetection");
+                this.$store.commit(SET_IDENTITY, this.userRadio);
+                this.$emit("gotoHome");
+              }
+            }).catch(err => {
+              // console.log(err)
+              this.$Message.error('请检查用户编号/密码');
+            })
+          }else{
+            const username = this.formInline.user;
+            const pwd = this.formInline.password;
+            getPropertyLoginformationName(username,pwd).then(res =>{
+              if(res.data.password == this.formInline.password){
+                // console.log("员工登录成功");
+                this.$router.push("/flowdetection");
+                this.$store.commit(SET_IDENTITY, this.userRadio);
+                this.$emit("gotoHome");
+              }
+            }).catch(err => {
+              // console.log(err)
+              this.$Message.error('请检查用户名/密码');
+            })
+          }
+        }
+      }
 
+    },
+  },
+};
 </script>
 <style>
 
