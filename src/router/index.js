e404943e92eecb3645b2c8f3b527cad7e2ecffac
tickurt <15034064978@163.com>
2022-04-21 17:41:56 +0800

    INIT: New src of /index.js

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+//导入路由配置
+import routes from "./routes"; 
+
+Vue.use(VueRouter);
+
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err);
+};
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
