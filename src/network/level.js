cb1dea0212cdc1f5d6aa6e55f325a5bf1efd4ba2
tickurt <15034064978@163.com>
2022-04-25 19:19:57 +0800

    INIT: network/level.js

---
+++ src/network/level.js
@@ -0,0 +1,23 @@
+import { request } from "./request";
+/**
+ * 疫情防控等级调整
+ * promise
+ */
+ export function getLevel() {
+  return request({
+    url: "/custom/epidemicLevel",
+    method: 'get',
+  });
+}
+ export function updataLevel({
+  code
+  }) {
+    // console.log(code);
+    return request({
+      url: `/custom/epidemicLevel?level=${code}`,
+      method: 'put',
+      // data: { //传给后端的json,参数根据具体信息修改
+      //   'code': level,
+      // }
+    })
+  }
