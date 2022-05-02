8584bd9d5cfea2c17ad4516a3a7c2e5a11eced51
Aric-Sun <zsunqian@foxmail.com>
2022-05-02 14:59:16 +0800

    add call for interface of getting owner by id

--- src/network/owner.js
+++ src/network/owner.js
@@ -8,4 +8,10 @@ export function getOwners(page, size) {
     params: { page, size }
   });
 }
-
+// 获取指定编号的业主具体信息
+export function getOwnerInfo(id) {
+  return request({
+    url: `/usersInfo/${id}`,
+    method: 'GET'
+  });
+}
