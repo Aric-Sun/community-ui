6a9fe6753860f60ddab108ac6a3b95fd53502eec
Aric-Sun <zsunqian@foxmail.com>
2022-05-27 01:45:01 +0800

    add string functions

--- src/common/utils.js
+++ src/common/utils.js
@@ -121,3 +121,31 @@ export default function checkPermission(value) {
         return false
     }
 }
+export const toUpperCase = (str) => {
+    if (str[0]) {
+        return str.replace(str[0], str[0].toUpperCase())
+    } else {
+        return ''
+    }
+}
+
+export const toLowerCase = (str) => {
+    if (str[0]) {
+        return str.replace(str[0], str[0].toLowerCase())
+    } else {
+        return ''
+    }
+}
+
+// 驼峰转换下划线
+export const toSQLLine = (str) => {
+    if (str === 'ID') return 'ID'
+    return str.replace(/([A-Z])/g, "_$1").toLowerCase();
+}
+
+// 下划线转换驼峰
+export const toHump = (name) => {
+    return name.replace(/\_(\w)/g, function(all, letter) {
+        return letter.toUpperCase();
+    });
+}
