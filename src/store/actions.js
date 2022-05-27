bcd5a5bdc3122fcf006180e89db7c41fc6114208
tickurt <15034064978@163.com>
2022-05-27 17:34:09 +0800

    INIT about actions.js update

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,23 @@
 import {} from "./actions_type";
 
 export const actions = {
+	
+	async getUserInfo({
+		commit,
+		state
+	}) {
+		let res = await getUser();
+		commit(GET_USERINFO, res)
+	},
+	async saveAddress({
+		commit,
+		state
+	}) {
+
+		if(state.removeAddress.length > 0) return;
+
+		let addres = await getAddressList(state.userInfo.user_id);
+		commit(SAVE_ADDRESS, addres);	
+	},
 
 }
