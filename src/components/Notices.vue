7d6a1f9d7bfd15567dbc7c2d7dbc233028ad6bdc
tickurt <15034064978@163.com>
2022-05-30 19:23:37 +0800

    INIT: src of Notices.vue

---
+++ src/components/Notices.vue
@@ -0,0 +1,14 @@
+<template>
+  <div ref='noticeSelfs' :class="['noticeSelf' , classStyle]"  flex>
+    <div class='imgs'>
+      <img class="imgs1" src="../../../../assets/index/welcome.png" alt="">
+      <img class="imgs2" src="../../../../assets/index/headBg.png" alt="">
+    </div>
+    <div class='contents'>
+      <div v-if='showDeletes' @click='deletes'> 删除</div>
+      <div v-if='title' class='title'>{{title}}</div>
+      <div v-if='message' class='message'>{{message}}</div>
+      <render-cell v-if='renders' :render="renders"></render-cell>
+    </div>
+  </div>
+</template>
