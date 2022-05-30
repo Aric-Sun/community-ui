e96cf4a010929b37e77688087c5912a87c264a92
Aric-Sun <zsunqian@foxmail.com>
2022-05-31 00:54:53 +0800

    remove extra semicolon

--- src/common/utils.js
+++ src/common/utils.js
@@ -79,7 +79,7 @@ var loadUtil = {
 export function CheckPhone (s) {
         var patrn = /^(13[0-9]|14[0-9]|15[0-9]|17[0-9]|18[0-9])\d{8}$/;
         return patrn.exec(s);
-    };
+    }
 
 /**
  * 获取msite商铺列表
