65377b2b4eb49a19f6a7f3614d16e66bb8cd07ae
tickurt <15034064978@163.com>
2022-05-31 17:43:44 +0800

    INIT: Upadate of notices.vue

--- src/components/Notices.vue
+++ src/components/Notices.vue
@@ -12,3 +12,76 @@
     </div>
   </div>
 </template>
+<script>
+  import RenderCell from '@/libs/render';
+
+  export default {
+    components: {
+      RenderCell
+    },
+    props: {
+      classStyle: {
+        type: String,
+      },
+      showDeletes: { //是否显示删除按钮
+        type: Boolean,
+        default: true
+      },
+      title: { //标题
+        type: String,
+      },
+      message: { //message
+        type: String,
+      },
+      duration: { //延迟自动关闭时间 0 默认不自动关闭
+        type: Number,
+        default: 3000,
+      },
+      renders: {
+        type: Function
+      },
+      onclose: { //关闭是回调方法
+        type: Function
+      },
+    },
+    data() {
+      return {
+        timer: ''
+      };
+    },
+    mounted() {
+      //主动触发
+      this.show()
+    },
+    methods: {
+      //自定义关闭事件
+      close() {
+        this.onclose ? this.onclose() : '';
+        this.hide()
+      },
+      //主动删除
+      deletes() {
+        this.close()
+      },
+      //被动触发
+      show() {
+        if (this.duration != 0) {
+          this.timer = setTimeout(() => {
+            this.hide();
+          }, this.duration);
+        }
+      },
+      //关闭
+      hide() {
+        let dom = this.$refs.noticeSelfs
+        dom.setAttribute("class",dom.getAttribute('class')+" notice1-leave")
+        clearTimeout(this.timer)
+        let times = setTimeout(()=>{
+          clearTimeout(times)
+          this.remove();
+        },500)
+      },
+    },
+  };
+
+</script>
