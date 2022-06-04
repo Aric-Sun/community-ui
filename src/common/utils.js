8e7b4beb6a95fb979b114bfab4375802e1a5ec8f
Aric-Sun <zsunqian@foxmail.com>
2022-06-05 02:45:08 +0800

    add string function: Underline and hump converter

--- src/common/utils.js
+++ src/common/utils.js
@@ -192,3 +192,15 @@ export const toLowerCase = (str) => {
     }
 }
 
+// 驼峰转换下划线
+export const toSQLLine = (str) => {
+    if (str === 'ID') return 'ID'
+    return str.replace(/([A-Z])/g, "_$1").toLowerCase();
+}
+
+// 下划线转换驼峰
+export const toHump = (name) => {
+    return name.replace(/\_(\w)/g, function(all, letter) {
+        return letter.toUpperCase();
+    });
+}
